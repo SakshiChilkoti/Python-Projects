fde274c3f6cc5e8aabdf3e245a29f0c8eeeac1fd
Sakshi Chilkoti <145189160+SakshiChilkoti@users.noreply.github.com>
2024-05-17 12:02:25 +0530

    Add files via upload

---
+++ DiceRollStimulator.py
@@ -0,0 +1,22 @@
+
+import random
+
+
+minimum = 1
+maximum = 6
+
+
+Diceroll_again = "yes"
+
+while Diceroll_again == "yes" or Diceroll_again == "y":
+    print("Rolling your Dice...")
+    print("The Values are :")
+    
+    2
+    
+    print(random.randint(minimum,maximum))
+    
+
+    print(random.randint(minimum,maximum))
+    
+    Diceroll_again = input("Do you want to roll the Dice Again? ")
